3cf71718541398b28d8bbce8beabdcc4d3724c51
mostafa-cse <mostafakamal.cse2022@gmail.com>
2026-09-09 13:53:19 +0600

    Switch to new Supabase project (pmwvigufadlianjinxvk)

--- supabase-config.js
+++ supabase-config.js
@@ -15,5 +15,5 @@
  * works and progress is kept in this browser, but there are no accounts and
  * nothing syncs between devices.
  */
-window.SUPABASE_URL = "https://whddpmsofaborfadiyss.supabase.co";
-window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndoZGRwbXNvZmFib3JmYWRpeXNzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU4NjEyOTksImV4cCI6MjEwMTQzNzI5OX0.3aG-pcmZRFmg4MkTZ3DCNMTT0mTNjmdisP3lBtmWAF8";
+window.SUPABASE_URL = "https://pmwvigufadlianjinxvk.supabase.co";
+window.SUPABASE_ANON_KEY = "sb_publishable_X9nryKf9hOoHYo5AzSGheA_Y5A9Zgxl";
